c9c90ef20cd437519379796fb561384a2a98d3c8
rashadjamal <119690146+rashadjamal@users.noreply.github.com>
2022-12-03 20:23:13 +0530

    Add files via upload

---
+++ even odd number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class cheakeven
+{
+ public static void main (String args[])
+ {
+	 int n;
+	 System.out.println("enter any number");
+	 Scanner obj=new Scanner(System.in);
+	 n=obj.nextInt();
+	 if(n%2==0)
+	 {
+		 System.out.println("even number");
+	 }
+	 else
+	 {
+		 System.out.println("odd number");
+	 }
+	 }
+}
+	 
+	 
