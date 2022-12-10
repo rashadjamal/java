c34065f699c66789fe5e7e47ab01c286e77f6f13
rashadjamal <119690146+rashadjamal@users.noreply.github.com>
2022-12-10 09:31:02 +0530

    Add files via upload

---
+++ day9/command line.java
@@ -0,0 +1,11 @@
+             Class command1
+             {
+                 public static void main(String[] args)
+                 {
+                      for(String str: args) 
+                      {
+                            int argument = Integer.parseInt(str);
+                            System.out.println("Argument in integer form: " + argument);
+                       }
+                 }
+            }
