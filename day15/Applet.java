ff2c5d4b58cc5a1217e402e18f169c29fadeb5c3
rashadjamal <119690146+rashadjamal@users.noreply.github.com>
2022-12-16 20:01:15 +0530

    Add files via upload

---
+++ day15/Applet.java
@@ -0,0 +1,16 @@
+                          import java.applet.Applet;
+                         import java.awt.Graphics;
+ /*
+<applet code="HelloWorld" width=200 height=60>
+</applet>
+ 
+public class HelloWorld extends Applet
+{
+    // Overriding paint() method
+    @Override
+    public void paint(Graphics g)
+    {
+        g.drawString("Hello World", 20, 20);
+    }
+     }
+
