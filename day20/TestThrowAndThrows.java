b3d7d88315c56052da147786050116b9b2f36b56
rashadjamal <119690146+rashadjamal@users.noreply.github.com>
2022-12-21 10:14:40 +0530

    Add files via upload

---
+++ day20/TestThrowAndThrows.java
@@ -0,0 +1,22 @@
+              public class TestThrowAndThrows  
+              {    
+                     static void method() throws ArithmeticException  
+                      {  
+                             System.out.println("Inside the method()");  
+                             throw new ArithmeticException("throwing ArithmeticException");  
+                       }  
+      
+                        public static void main(String args[])  
+                         {  
+                               try  
+                                  {  
+                                          method();  
+                                    }  
+                                    catch(ArithmeticException e)  
+                                    {  
+                                                System.out.println("caught in main() method");  
+                                      }  
+                                }  
+                           }  
+
+ 
