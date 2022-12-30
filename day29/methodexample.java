93347aaec179c383f362983f430f921f40916be1
rashadjamal <119690146+rashadjamal@users.noreply.github.com>
2022-12-30 11:06:11 +0530

    Add files via upload

---
+++ day29/methodexample.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class methodexample
+{
+   public static void printSum(int a,int b)
+   {
+   int  sum=a+b;
+   System.out.println(sum);
+   }
+  public static void main(String []args)
+  {
+    printSum(10,20);
+	
+	
+	}
+}
+	
+	
+
+
+
+		
---
+++ day29/ras.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ras
+{
+  public static void main(String []args)
+  {
+    int[] marks= {45,30,47};
+	try
+	{
+	System.out.println(marks[5]);
+	}
+	catch(Exception Exception)
+	{
+	}
+	System.out.println("this is rashad");
+	}	   
+  }
+
+
+
+		
