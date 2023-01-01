3a77a6942503ef124d06eb3840f6833f6c6362c1
rashadjamal <119690146+rashadjamal@users.noreply.github.com>
2023-01-01 12:15:51 +0530

    Add files via upload

---
+++ day31/mthod exm.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class metfun
+{
+   public static void printjava()
+   {
+   
+   System.out.println("this is rashad");
+   }
+  public static void main(String []args)
+  {
+    printjava();
+	printjava();
+	printjava();
+	
+	}
+}
+	
+	
+
+
+
+		
---
+++ day31/string.java
@@ -0,0 +1,11 @@
+public class ayan
+{
+       public static void main (String args[])
+	   {
+	      
+		   
+		    String name= "rashad and affan";
+			  System.out.println(name.substring(7,11));
+		   
+		 }
+} 	
