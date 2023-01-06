aa66afdcd8ebbc59778624578f77385c2a2be889
rashadjamal <119690146+rashadjamal@users.noreply.github.com>
2023-01-06 15:06:49 +0530

    Add files via upload

---
+++ day36/IfElseTernary.java
@@ -0,0 +1,7 @@
+public class IfElseTernary {    
+public static void main(String[] args) {    
+    int number=13;      
+    String output=(number%2==0)?"even number":"odd number";    
+    System.out.println(output);  
+}    
+}    
