2a3e91ccab64a711ac07fa8529d7c4ec44db4f10
rashadjamal <119690146+rashadjamal@users.noreply.github.com>
2023-01-11 20:09:58 +0530

    Add files via upload

---
+++ day41/TestStudent.java
@@ -0,0 +1,13 @@
+//Java Program to demonstrate having the main method in     
+//Creating Student class.  
+class Student{  
+ int id;  
+ String name;  
+}  
+ public class TestStudent1{  
+ public static void main(String args[]){  
+  Student s1=new Student();  
+  System.out.println(s1.id);  
+  System.out.println(s1.name);  
+ }  
+}  
