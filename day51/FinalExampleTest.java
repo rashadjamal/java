8ef254377541872f6caf0959a5d90cc2c72178c8
rashadjamal <119690146+rashadjamal@users.noreply.github.com>
2023-01-21 10:29:30 +0530

    Add files via upload

---
+++ day51/FinalExampleTest.java
@@ -0,0 +1,12 @@
+public class FinalExampleTest {  
+    final int age = 18;  
+    void display() {  
+  int  age = 55;  
+    }  
+      
+    public static void main(String[] args) {  
+      
+    FinalExampleTest obj = new FinalExampleTest();  
+    obj.display();  
+    }  
+}  
