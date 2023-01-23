fc2c218a7d6a81d31b791fd7e16cf5f7b9060ce3
rashadjamal <119690146+rashadjamal@users.noreply.github.com>
2023-01-23 10:44:20 +0530

    Add files via upload

---
+++ 53/TestThrows.java
@@ -0,0 +1,17 @@
+public class TestThrows {   
+    public static int divideNum(int m, int n) throws ArithmeticException {  
+        int div = m / n;  
+        return div;  
+    }  
+    public static void main(String[] args) {  
+        TestThrows obj = new TestThrows();  
+        try {  
+            System.out.println(obj.divideNum(45, 0));  
+        }  
+        catch (ArithmeticException e){  
+            System.out.println("\nNumber cannot be divided by 0");  
+        }  
+          
+        System.out.println("Rest of the code..");  
+    }  
+}  
