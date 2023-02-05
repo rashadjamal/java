eff6f8fcc74de2f2966e3e31b7310e0cd8c65d89
rashadjamal <119690146+rashadjamal@users.noreply.github.com>
2023-02-06 00:06:41 +0530

    Add files via upload

---
+++ 67/VolumeOfPrism2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class VolumeOfPrism2
+{
+   public static void main(String args[]) 
+    {   
+        
+        
+            double base=Double.parseDouble(args[0]);  
+
+            double height=Double.parseDouble(args[1]);
+ 
+            double  area=base*height;
+
+            System.out.println("Area of prism is: " + area);
+          
+   }
+}
